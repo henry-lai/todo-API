b8199d0f59d4d1a4928a0c76169993e470f7440a
henry.lai1 <henry.lai@n-hop.com>
2023-01-17 18:02:10 +0800

    added chages

--- NJs-todoapp/routes/index.js
+++ NJs-todoapp/routes/index.js
@@ -1,22 +1,36 @@
 const router = require("express").Router();
+const { v4: uuidv4 } = require('uuid');
+
+
 
 const todoItems = [
-   { Name: "Henry",Description: "Lorem Ipsum",Due_Date: "2022-01-07", Status: "Done"},
-   { Name: "Henry2",Description: "Lorem Ipsum2",Due_Date: "2022-01-08", Status: "NotDone"}
+	{
+		name: "Henry",
+		description: "Lorem Ipsum",
+		dueDate: "2022-01-07",
+		status: "Done"
+	},
+   	{ 
+		name: "Henry2",
+		description: "Lorem Ipsum2",
+		dueDate: "2022-01-08", 
+		status: "NotDone"
+	}
 ]
-router.get("/", (req,res) => {
-   res.json({
-      message: "Welcome!",
-   });
-   // res.render("index");
+
+router.get('/', (req,res) => {
+	//console.log(todoItems);
+
+	res.send(todoItems);
 });
 
-router.get('/items', (req,res) =>{
-   console.log(todoItems);
+router.post('/', (req,res) =>{
+	const todoEntry = req.body;
+	const userId = uuidv4(); 
+	const userWithId = {}
+	todoItems.push(todoEntry);
+	res.send(`user with the name ${todoEntry.name} added`);
+})
 
-   res.json({
-      todoItems,
-   });
-});
-
+//export default router;
 module.exports = router;
